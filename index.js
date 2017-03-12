7f6c55e59e6e6284b03b10363d52147b7bcc5a09
Blimeys <djeanmath@gmail.com>
2017-03-12 15:52:30 +0000

    Find data for a specific station using a factory - completed

--- index.js
+++ index.js
@@ -7,6 +7,9 @@ app.use('/scripts',express.static(__dirname + '/scripts'));
 app.get('/', function(req,res){
   res.sendFile(__dirname + '/index.html')
 });
+app.get('/tubeUsers.json', function(req,res){
+  res.sendFile(__dirname + '/tubeUsers.json')
+});
 
 
 app.listen(3000, function(){
--- scripts/controller.js
+++ scripts/controller.js
@@ -1,6 +1,25 @@
 var mainApp = angular.module('mainApp', []);
 
 
-mainApp.controller('stuff', ['$scope', function($scope){
+mainApp.factory('jsonQueryStations', ['$filter', '$http', function($filter,$http){
+  var station = {};
+  return {
+    stationData: function(){
+    return $http.get('tubeUsers.json')
+      .then(function(results){
+        var currentStation = "Baker Street";
+        return {station: $filter('filter')(results.data, {Station: currentStation })[0]};
+  });
+      return {
+        station: station
+      }
+  }
+}
+}]);
+
+mainApp.controller('stuff', ['$scope', 'jsonQueryStations', function($scope,jsonQueryStations){
   $scope.testCtr = 'Test World';
+  $scope.stationResult = jsonQueryStations.stationData().then(function(result){
+    console.log(result.station);
+  });
 }]);
