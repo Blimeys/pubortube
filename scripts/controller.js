511e423b9ad3720a8e2ec905294ca14d0433927f
Blimeys <djeanmath@gmail.com>
2017-03-13 12:16:01 +0000

    finished the current time finder and formater

--- scripts/controller.js
+++ scripts/controller.js
@@ -16,10 +16,59 @@ mainApp.factory('jsonQueryStations', ['$filter', '$http', function($filter,$http
   }
 }
 }]);
+mainApp.factory('timeConverter', [function(){
+  var currentTime = "";
+  var getTimeConverter = function findTimeStringIt (){
+            var hour = new Date().getHours();
+        var minute = new Date().getMinutes();
+        if (minute<16){
+          var minuteLower = 0;
+          var minuteUpper = 15;
+          if (minuteLower === 0){
+            var currentTime = hour.toString() + minuteLower.toString() + "0-" + hour.toString() + minuteUpper.toString();
+            return currentTime;
+          } else {
+          var currentTime = hour.toString() + minuteLower.toString() + "-" + hour.toString() + minuteUpper.toString();
+          return currentTime;
+          }
+        } else if (minute>15 && minute<31){
+          var minuteLower = 16;
+          var minuteUpper = 30;
+          var currentTime = hour.toString() + minuteLower.toString() + "-" + hour.toString() + minuteUpper.toString();
+          return currentTime;
+        } else if (minute>30 && minute<46){
+          var minuteLower = 31;
+          var minuteUpper = 45;
+          var currentTime = hour.toString() + minuteLower.toString() + "-" + hour.toString() + minuteUpper.toString();
+          return currentTime;
+        } else if (minute>45 && minute<=59){
+          var minuteLower = 46;
+          var minuteUpper = 59;
+          var currentTime = hour.toString() + minuteLower.toString() + "-" + hour.toString() + minuteUpper.toString();
+          return currentTime;
+        }
+  }
 
-mainApp.controller('stuff', ['$scope', 'jsonQueryStations', function($scope,jsonQueryStations){
+
+  return {
+    getTime: function timeData(){
+      return getTimeConverter();
+
+    }
+  }
+
+}])
+
+
+mainApp.controller('stuff', ['$scope', 'jsonQueryStations', 'timeConverter', function($scope,jsonQueryStations, timeConverter){
   $scope.testCtr = 'Test World';
   $scope.stationResult = jsonQueryStations.stationData().then(function(result){
     console.log(result.station);
   });
+  $scope.hour = new Date().getHours().toString() + new Date().getMinutes().toString();
+  $scope.minute = new Date().getMinutes().toString();
+/*  $scope.timeNow = timeConverter.getTimeConverter().then(function(timeresult){
+    console.log(timeresult.currentTime);*/
+    var findthetime = timeConverter.getTime();
+  $scope.timeNow = console.log(findthetime);
 }]);
