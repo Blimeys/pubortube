8dd3e9d1b2dd814ef76eb5f362875138e3b83da1
Blimeys <djeanmath@gmail.com>
2017-03-24 15:10:07 +0000

    Cleaning up, working on layout

--- scripts/controller.js
+++ scripts/controller.js
@@ -10,11 +10,13 @@ stationStatus.controller('stationEntry', ['$scope', 'jsonQueryStations', 'timeCo
           if ($scope.selectedStation == null || $scope.selectedStation == undefined){
             $scope.noStation = "Please enter a station name";
           } else if (result.station[findTheTime] > result.station.Average) {
-            $scope.stationName = result.station.Station + " is busy";
+            $scope.stationName = result.station.Station;
+            $scope.stationResults = result.station.Station + " is busy, go to the pub";
             $scope.icon = "typcn typcn-beer";
             console.log(result.station);
           } else if (result.station[findTheTime] <= result.station.Average){
-            $scope.stationName = result.station.Station + " is quiet";
+            $scope.stationName = result.station.Station;
+            $scope.stationResults = result.station.Station + " is quiet, go home";
             $scope.icon = "typcn typcn-home";
             console.log(result.station);
             }
